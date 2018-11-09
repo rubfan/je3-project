122cef382072d50fada5bfb2cee0ecf5ac02132f
Mazovskiy <42416483+Mazovskiy@users.noreply.github.com>
2018-11-10 00:53:25 +0200

    upgrade_product SQL for Table done

--- src/main/resources/db/06_Upgrade.sql
+++ src/main/resources/db/06_Upgrade.sql
@@ -1,25 +1,33 @@
-INSERT INTO `building`
+INSERT INTO `upgrade`
     (`id`, `name`,`description`)
 VALUES
-    ('1', 'Granary', 'The most mysterious building. It makes food from nothing.'),
-    ('2', 'Chicken House', 'Produce eggs'),
-    ('3', 'Incubator', 'Produce chickens'),
-    ('4', 'Cleaning Center', 'Produce dung'),
-    ('5', 'Chicken Yard', 'Produce hens'),
-    ('6', 'Cocks House', 'Produce cocks'),
-    ('7', 'Cocks Barrack', 'Produce fighting cocks'),
-    ('8', 'Quarry', 'Produce stone'),
-    ('9', 'School of Hammers', 'Produce cocks with hammers'),
-    ('10', 'School of Slingshots', 'Produce cocks with slingshots'),
-    ('11', 'Blast Furnace', 'Produce steel'),
-    ('12', 'School of Swordsmen', 'Produce cocks swordsmen (in lats)'),
-    ('13', 'School of Axes', 'Produce cocks with axes (in lats)'),
-    ('14', 'Logger', 'Produce wood'),
-    ('15', 'School of Archers', 'Produce cocks archers'),
-    ('16', 'School of Lancers', 'Produce cocks lancers'),
-    ('17', 'Alchemy Laboratory', 'Produce gunpowder'),
-    ('18', 'School of Gunners', 'Produce cocks gunners'),
-    ('19', 'School of Bazookas', 'Produce cocks with bazookas'),
-    ('20', 'Altar', 'Produce mana essence'),
-    ('21', 'School of Magic', 'Produce cocks magicians'),
-    ('22', 'Zikkurat', 'Produce cocks prophets')
+    ('1', 'Upgrade Granary', 'The most mysterious building. It makes food from nothing.'),
+    ('2', 'Upgrade Granary and chicken house', 'Produce eggs'),
+    ('3', 'Initialize the chickens', 'Produce chickens'),
+    ('4', 'F@ck this, i''m out', 'Produce dung'),
+    ('5', 'Glue a chickens', 'Produce hens'),
+    ('6', 'Ruffle up a chickens', 'Produce cocks'),
+    ('7', 'Mother-in-law? I''m in the army', 'Produce fighting cocks'),
+    ('8', 'Minecraft?', 'Produce stone'),
+    ('9', 'USSC', 'Produce cocks with hammers'),
+    ('10', 'Sling? Nooooo!!!', 'Produce cocks with slingshots'),
+    ('11', 'Steelworkers', 'Produce steel'),
+    ('12', 'Chickens infantry', 'Produce cocks swordsmen (in lats)'),
+    ('13', 'Clumsy fame', 'Produce cocks with axes (in lats)'),
+    ('14', 'New saws', 'Produce wood'),
+    ('15', 'Add optical sight to all bow', 'Produce cocks archers'),
+    ('16', 'Training of lancers', 'Produce cocks lancers'),
+    ('17', 'Crazy professor', 'Produce gunpowder'),
+    ('18', 'Add fire transfer pen', 'Produce cocks gunners'),
+    ('19', 'ChiRambo', 'Produce cocks with bazookas'),
+    ('20', 'Meditation', 'Produce mana essence'),
+    ('21', 'Kill the Witcher', 'Produce cocks magicians'),
+    ('22', 'Life for Ner''Zul', 'Produce cocks prophets')
+    ('23', 'Pesticides', 'Produce cocks prophets')
+    ('24', 'Broiler', 'Produce cocks prophets')
+    ('25', 'Red bull', 'Produce cocks prophets')
+    ('26', 'Muscle gym', 'Produce cocks prophets')
+    ('27', 'New armor', 'Produce cocks prophets')
+    ('28', 'Steel feathers', 'Produce cocks prophets')
+    ('29', 'Armed and dangerous', 'Produce cocks prophets')
+    ('30', 'Disney Princess? Really?', 'Produce cocks prophets')
