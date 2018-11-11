a8569228281f3b71b79e93b4449947b87b266052
Ruslan Gramatic <ruslan.gramatic@opower.com>
2018-11-12 01:56:12 +0200

    Change server port

--- src/main/java/ua/od/game/ApplicationStarter.java
+++ src/main/java/ua/od/game/ApplicationStarter.java
@@ -11,13 +11,16 @@ import org.eclipse.jetty.server.Server;
  */
 public class ApplicationStarter {
     public static void main(String[] args) throws Exception {
+        Server jettyServer;
         if (args != null && args.length > 0 && args[0].equals("deploy")) {
             DataBaseDeployer.createDb();
             DataBaseDeployer.deployDb();
+            jettyServer = new Server(Integer.valueOf(System.getenv("PORT")));
+        } else {
+            jettyServer = new Server(9090);
         }
-        boolean debug = false;
 
-        Server jettyServer = new Server(9090);
+        boolean debug = false;
         jettyServer.setHandler(new AppContextConfig().getHandlersConfig());
         try {
             jettyServer.start();
