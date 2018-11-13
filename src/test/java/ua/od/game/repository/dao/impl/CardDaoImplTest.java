305ece92415f841edab8582a351df496de2083e5
Ruslan Gramatic <ruslan.gramatic@opower.com>
2018-11-13 10:56:17 +0200

    Returned card dao and card dao tests

---
+++ src/test/java/ua/od/game/repository/dao/impl/CardDaoImplTest.java
@@ -0,0 +1,32 @@
+package ua.od.game.repository.dao.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.List;
+
+import ua.od.game.model.CardEntity;
+import ua.od.game.repository.dao.DbTest;
+
+/**
+ * @author DemianSH
+ **/
+
+public class CardDaoImplTest extends DbTest {
+
+    CardDaoImpl cardDao;
+
+    @Before
+    public void init() {
+        cardDao = new CardDaoImpl();
+    }
+
+    @Test
+    public void getAllCardListtest() {
+        List<CardEntity> cardsTest = cardDao.getAllCardList();
+        Assert.assertFalse(cardsTest.isEmpty());
+        Assert.assertEquals("Granary", cardsTest.get(0).getName());
+        Assert.assertEquals("Chicken House", cardsTest.get(1).getName());
+    }
+}
