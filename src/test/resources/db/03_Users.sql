f8dcc16863babe18cb731bc0a6799b17124fa22b
root <root@centos.(none)>
2018-11-15 10:54:18 +0200

    Users Upgrade Dao

--- src/test/resources/db/03_Users.sql
+++ src/test/resources/db/03_Users.sql
@@ -1,8 +1,7 @@
 INSERT INTO `User`
-    (`name`,`password`,`token`)
+    (`id`,`name`,`password`,`token`)
 VALUES
-    ('ramzes','12345','6789'),
-    ('bob','6789','7777'),
-    ('Scripa4','11111','7777'),
-    ('king','44444','7778')
-
+    (1,'ramzes','12345','6789'),
+    (2,'bob','6789','7777'),
+    (3,'Scripa4','11111','7777'),
+    (4,'king','44444','7778');
