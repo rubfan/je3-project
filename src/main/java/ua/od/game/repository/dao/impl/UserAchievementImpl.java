de9b43fcc4f39a8dfb245795918b68f42758a9db
Alex <flotcka@gmail.com>
2018-11-16 23:04:47 +0200

    commit without tests

--- src/main/java/ua/od/game/repository/dao/impl/UserAchievementImpl.java
+++ src/main/java/ua/od/game/repository/dao/impl/UserAchievementImpl.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class UserAchievementImpl implements UserAchievementDAO {
 
+    @Override
     public List<AchievementNumberEntity> getUserNotifications(Integer userId) {
         return SqlHelper.prepareStatement("Select * from `User_Achievement` where user_id = ?", prpstmt -> {
             prpstmt.setInt(1, userId);
@@ -26,24 +27,3 @@ public class UserAchievementImpl implements UserAchievementDAO {
     }
 }
 
-/*
-Создать ua.od.game.repository.dao.UserAchievementDAO class
-в котором будут методы:
-
-method: List<AchievementNumberEntity> getUserNotifications(Integer userId)
-description: метод должен возвращать список обьектов AchievementNumberEntity
-в котором будут заполнены из базы 2 поля:
-id - айди ачивмента юзера
-number - количество ачивментов
-
-Database script:
-CREATE TABLE User_Achievement (
-id INT NOT NULL AUTO_INCREMENT,
-user_id INT NOT NULL,
-achievement_id INT NOT NULL,
-number FLOAT NOT NULL,
-PRIMARY KEY (id)
-);
-
-
- */
