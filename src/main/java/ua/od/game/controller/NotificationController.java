7a7dfa2d6041a096ea2dd2307f7fe132ce570396
AleksandrPashkevich <42419120+AleksandrPashkevich@users.noreply.github.com>
2018-12-03 07:41:21 +0200

    Update NotificationController.java

--- src/main/java/ua/od/game/controller/NotificationController.java
+++ src/main/java/ua/od/game/controller/NotificationController.java
@@ -2,14 +2,11 @@ package ua.od.game.controller;
 
 import ua.od.game.dto.NotificationDto;
 
-import javax.ws.rs.core.Response;
 import java.util.List;
 
 /**
  * @author ruslan.gramatic
  */
 public interface NotificationController {
-
     List<NotificationDto> getAllNotificationList();
-
 }
